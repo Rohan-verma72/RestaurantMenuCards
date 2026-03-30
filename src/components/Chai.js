0360a4e1353a26573992306f75e93ee55e07b7dc
Rohan Verma <rohan.verma@example.com>
2026-03-30 14:16:29 +0530

    Ready to deploy - React build

---
+++ src/components/Chai.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { chai } from "./utils/chai";
+import { Item } from "./Item";
+
+const Chai = () => {
+  return (
+    <div>
+      {chai.map((item) => (
+        <div>
+          <Item
+            name={item.name}
+            img={item.img}
+            price={item.price}
+            description={item.description}
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
+export default Chai;
